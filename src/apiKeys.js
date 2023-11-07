c31a1b3202b92c8e171420ce5502ce0e5b394691
Suchit Gaidhane <131668852+SuchitGaidhane@users.noreply.github.com>
2023-11-07 12:00:19 +0530

    Update apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "368bcc13de3942b9097b4f989eea0cc9",
+  key: "{Enter your API keys}",
   base: "https://api.openweathermap.org/data/2.5/",
 };
